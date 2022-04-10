9f0c6ef1c9788308abfbc17c317784bca5fc6eac
RomanQed <gbakaldin@gmail.com>
2022-04-10 23:33:16 +0300

    Fix missing encoding header

--- src/main/java/io/github/amayaframework/core/actions/ProcessHeadersAction.java
+++ src/main/java/io/github/amayaframework/core/actions/ProcessHeadersAction.java
@@ -1,10 +1,12 @@
 package io.github.amayaframework.core.actions;
 
+import io.github.amayaframework.core.config.ConfigProvider;
 import io.github.amayaframework.core.contexts.ContentType;
 import io.github.amayaframework.core.contexts.HttpResponse;
 import io.github.amayaframework.core.pipeline.PipelineAction;
 
 import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.Charset;
 
 /**
  * <p>The output action during which the response headers is sent.</p>
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
  * <p>Returns: {@link ServletResponseData}</p>
  */
 public class ProcessHeadersAction extends PipelineAction<ServletResponseData, ServletResponseData> {
+    private final Charset charset = ConfigProvider.getConfig().getCharset();
+
     @Override
     public ServletResponseData execute(ServletResponseData responseData) {
         HttpServletResponse servletResponse = responseData.servletResponse;
@@ -21,6 +25,7 @@ public class ProcessHeadersAction extends PipelineAction<ServletResponseData, Se
         ContentType type = response.getContentType();
         if (response.getBody() != null || (type != null && !type.isString())) {
             servletResponse.setContentType(type.getHeader());
+            servletResponse.setCharacterEncoding(charset.name());
         }
         response.getCookies().forEach(servletResponse::addCookie);
         return responseData;
