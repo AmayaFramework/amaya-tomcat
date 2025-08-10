36b1e927500965654137a718ed35958a2819e06c
Roman Bakaldin <gbakaldin@gmail.com>
2025-08-10 22:37:13 +0300

    Init project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module amayaframework.tomcat {
+    // Imports
+    // Basic dependencies
+    requires com.github.romanqed.jfunc;
+    requires com.github.romanqed.jsync;
+    requires com.github.romanqed.juni;
+    requires com.github.romanqed.jct;
+    requires com.github.romanqed.jtype;
+    // Jakarta servlets
+    requires jakarta.servlet;
+    // Tomcat dependencies
+    // Amaya dependencies
+    requires amayaframework.options;
+    requires amayaframework.server;
+    requires amayaframework.service;
+    requires static amayaframework.environment;
+    // Exports
+    exports io.github.amayaframework.tomcat;
+}
