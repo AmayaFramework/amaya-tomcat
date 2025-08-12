1223df397fced8048e10f588c3185e6ebcaf1ccf
Roman Bakaldin <gbakaldin@gmail.com>
2025-08-13 00:22:47 +0300

    Add javadoc for TomcatFactory and TomcatContextConfigurer

--- src/main/java/io/github/amayaframework/tomcat/TomcatContextConfigurer.java
+++ src/main/java/io/github/amayaframework/tomcat/TomcatContextConfigurer.java
@@ -4,11 +4,27 @@ import io.github.amayaframework.environment.Environment;
 import io.github.amayaframework.options.OptionSet;
 import org.apache.catalina.Context;
 
+/**
+ * Configurer interface for customizing a {@link org.apache.catalina.Context}.
+ */
 public interface TomcatContextConfigurer {
 
+    /**
+     * Configures the given {@link Context} using the provided options and environment.
+     *
+     * @param context the {@link Context} to configure
+     * @param options the {@link OptionSet} with configuration parameters
+     * @param env     the {@link Environment} providing runtime context
+     */
     default void configure(Context context, OptionSet options, Environment env) {
         configure(context, options);
     }
 
+    /**
+     * Configures the given {@link Context} using the provided options.
+     *
+     * @param context the {@link Context} to configure
+     * @param options the {@link OptionSet} with configuration parameters
+     */
     void configure(Context context, OptionSet options);
 }
--- src/main/java/io/github/amayaframework/tomcat/TomcatFactory.java
+++ src/main/java/io/github/amayaframework/tomcat/TomcatFactory.java
@@ -4,11 +4,27 @@ import io.github.amayaframework.environment.Environment;
 import io.github.amayaframework.options.OptionSet;
 import org.apache.catalina.startup.Tomcat;
 
+/**
+ * Factory interface for creating configured {@link Tomcat} instances.
+ */
 public interface TomcatFactory {
 
+    /**
+     * Creates a new {@link Tomcat} instance using the given options and environment.
+     *
+     * @param options     the {@link OptionSet} with Tomcat configuration
+     * @param environment the {@link Environment} providing runtime context
+     * @return the created Tomcat instance
+     */
     default Tomcat create(OptionSet options, Environment environment) {
         return create(options);
     }
 
+    /**
+     * Creates a new {@link Tomcat} instance using the given options.
+     *
+     * @param options the {@link OptionSet} with Tomcat configuration
+     * @return the created Tomcat instance
+     */
     Tomcat create(OptionSet options);
 }
