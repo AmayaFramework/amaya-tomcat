aa4af2b39477be576863697f4e8b7d51e9c90d87
Roman Bakaldin <gbakaldin@gmail.com>
2025-08-15 12:21:34 +0300

    Add missing javadoc for module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,28 @@
+/**
+ * Provides integration between the Amaya Framework and the embedded
+ * Apache Tomcat server.
+ *
+ * <p>This module contains:
+ * <ul>
+ *     <li>Option keys and configuration helpers for Tomcat-based servers</li>
+ *     <li>Utilities for SSL, connector, and executor configuration</li>
+ *     <li>Support for optional WebSocket integration via the Tomcat WebSocket module</li>
+ *     <li>Session ID generators with pluggable random number sources</li>
+ * </ul>
+ *
+ * <p>Required dependencies include:
+ * <ul>
+ *     <li>Apache Tomcat embed core API</li>
+ *     <li>Jakarta Servlet API</li>
+ *     <li>Amaya Framework core modules (options, server, service)</li>
+ * </ul>
+ *
+ * <p>Optional dependencies include:
+ * <ul>
+ *     <li>Amaya environment module</li>
+ *     <li>Tomcat embedded WebSocket support</li>
+ * </ul>
+ */
 module amayaframework.tomcat {
     // Imports
     // Basic dependencies
